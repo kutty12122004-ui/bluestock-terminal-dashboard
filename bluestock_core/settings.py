f12df74e1e32720e59522868d4340e9b50ef2a6d
PALANIVEL R <kutty12122004@gmail.com>
2026-06-12 14:03:44 +0530

    Fix database settings for cloud compatibility

--- bluestock_core/settings.py
+++ bluestock_core/settings.py
@@ -90,12 +90,8 @@ WSGI_APPLICATION = 'bluestock_core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bluestock_dw',
-        'USER': 'postgres',
-        'PASSWORD': 'password',  # <-- MUST match the password used in your ETL script exactly!
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
